ca9ac2552c612cf818e9a6d9d1b65a1e4a05758b
ANDERSON ALVES LOPES <neno-alveslopes@hotmail.com>
2022-06-09 21:40:36 -0300

    Initial commit

---
+++ B7WEB_urna_eletronica/js/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+        {
+            numero:'38111',
+            nome:'Fulano de Tal',
+            partido: 'ABC',
+            fotos:[
+                {url:'33.02.jpg', legenda:'Vereador', small:false}
+            ]
+        },
+        {
+            numero: '77222',
+            nome:'Beltrano da Silva',
+            partido:'DEFG',
+            fotos:[
+                {url:'33.01.jpg', legenda: 'Vereador'}
+            ]
+        },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros:2,
+        candidatos:[
+           {
+               numero:'99',
+               nome:'Ciclano',
+               partido:'ABC',
+               vice:'Cic',
+               fotos:[
+                   {url:'11.01.jpg', legenda:'Prefeito'},
+                   {url:'22.01.jpg', legenda:'Vice-Prefeito', small:true}
+               ]
+
+           },
+           {
+               nunero:'84',
+               nome:'Zulano',
+               partido:'QWERTY',
+               vice:'Zul',
+               fotos:[
+                   {url:'22.02.jpg', legenda:'Prefeito'},
+                   {url:'11.02.jpg', legenda:'Vice-Prefeito', small:true}
+               ]
+           }, 
+        ]
+    }
+];
